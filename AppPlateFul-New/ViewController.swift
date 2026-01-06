3bdc802f230a90e28b44a4e5d61dc6e5adc08939
Haas7nn <hassan.subject2@gmail.com>
2026-01-06 16:22:43 +0300

    Initial Commit

---
+++ AppPlateFul-New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppPlateFul-New
+//
+//  Created by Hassan Fardan on 06/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
